aadf020e90067f13436817bb55bd8bf0d3420852
be116 <furikura06@outlook.com>
2018-11-18 11:41:01 +0900

    req

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,15 @@ var express = require('express');
 var os = require('os'); // ホスト名を取得するために使用
 var router = express.Router();
 
-var hostname = req.protocol + '://' + req.headers.host;
-
 /*=========== index =============*/
 router.get('/', function(req, res, next) {
+  var hostname = req.protocol + '://' + req.headers.host;
   res.render('index', { title: 'Playground', author: 'nerunerune', day1: hostname + 'day1'});
 });
 
 /*========== day 1 ===========*/
 router.get('/day1', function(req, res, next) {
+  var hostname = req.protocol + '://' + req.headers.host;
   res.render('day1', { title: 'Playground', author: 'nerunerune', day1: hostname + 'day1'});
 });
 
