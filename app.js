00f358e0c02fb7afeb1250ced7702d2c5c05ae6c
be116 <furikura06@outlook.com>
2018-11-18 16:36:31 +0900

    ss

--- app.js
+++ app.js
@@ -4,28 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var favicon = require('serve-favicon');
-var LEX = require('letsencrypt-express');
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
-// letsencrypt-express初期化
-
-var DOMAIN = 'playground.bbb-project.com';
-var EMAIL = 'be116.live@gmail.com';
-
-var lex = LEX.create({
-  configDir: require('os').homedir() + '/letsencrypt/etc'
-, approveRegistration: function (hostname, approve) { // leave `null` to disable automatic registration
-    if (hostname === DOMAIN) { // Or check a database or list of allowed domains
-      approve(null, {
-        domains: [DOMAIN]
-      , email: EMAIL
-      , agreeTos: true
-      });
-    }
-  }
-});
 
 var app = express();
 
@@ -57,14 +37,6 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
-});
-
-// letsモジュール実行
-lex.onRequest = app;
-
-lex.listen([80], [443, 5001], function () {
-  var protocol = ('requestCert' in this) ? 'https': 'http';
-  console.log("Listening at " + protocol + '://localhost:' + this.address().port);
 });
 
 module.exports = app;
