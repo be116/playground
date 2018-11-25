8ada3cc86defa8eced0856962509153f3234d1b2
be116 <furikura06@outlook.com>
2018-11-25 16:34:12 +0900

    author

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,12 @@ var router = express.Router();
 
 /*=========== index =============*/
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Playground', author: 'nerunerune', day1: 'day1'});
+  res.render('index', { title: 'Playground', author: 'bbb', day1: 'day1'});
 });
 
 /*========== day 1 ===========*/
 router.get('/day1', function(req, res, next) {
-  res.render('day1', { title: 'Playground', author: 'nerunerune', day1: 'day1'});
+  res.render('day1', { title: 'Playground', author: 'bbb', day1: 'day1'});
 });
 
 module.exports = router;
